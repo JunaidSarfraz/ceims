c6e60d1a60538fc1e5496dd481b4bbadaf667c40
JunaidSarfraz <juneed.sarfraz@gmail.com>
2015-10-23 20:59:26 +0500

    some mail fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,11 +73,10 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  config.action_mailer.default_url_options = { host: 'ceims.herokuapp.com' }
-  config.action_mailer.default_url_options = {:host => 'ceims.herokuapp.com', :protocol => 'http'}
-  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.default_url_options = { host: "ceims.herokuapp.com" }
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
