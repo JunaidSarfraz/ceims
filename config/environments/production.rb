84f026ccfc29bf284537972d26622f44a3695a96
JunaidSarfraz <juneed.sarfraz@gmail.com>
2015-10-23 20:43:47 +0500

    some fixes to send email in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,12 +74,14 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
   config.action_mailer.default_url_options = { host: 'ceims.herokuapp.com' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = {:host => 'ceims.herokuapp.com', :protocol => 'http'}
+  #config.action_mailer.delivery_method = :smtp
+  #config.action_mailer.raise_delivery_errors = false
+  #config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "www.gmail.com",
+    #domain: "www.gmail.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: "official.ceims@gmail.com",
