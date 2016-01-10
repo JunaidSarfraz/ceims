e036ad6146e160cef3ef8bc9a2f173c61f17f084
JunaidSarfraz <juneed.sarfraz@gmail.com>
2016-01-10 21:09:14 +0500

    Better Errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
   
   config.action_mailer.default_url_options = { host: "ceims.herokuapp.com" }
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   # config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
